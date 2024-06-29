1b2467ca8600a549bfc9f04ad44a3a5585f93821
unknown <ayushkhandelwal227@gamil.com>
2024-06-29 11:14:10 +0530

    Resolving issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import { Card } from '@/components/ui/card'
 
 import './App.css'
 
 function App() {
 
   return (
+    <>
    <Button>Click me</Button>
+   <Card> </Card>
+    </>
   )
 }
 
