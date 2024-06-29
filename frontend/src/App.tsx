3396ed4a9abd0d485c42b177e2b0c4625e11e706
JAIMIL NAGAR <nagarjaimil420@gmail.com>
2024-06-29 13:48:49 +0530

    navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './components/Signup';
 import Login from './components/Login';
 import './App.css';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Navbar />} />
       </Routes>
     </BrowserRouter>
   );
